78cb3e24aee6bd2c285f11c5ca99dc2d63fe1417
ilija <komperdel@gmail.com>
2022-08-08 15:58:04 +0300

    first commit

---
+++ src/components/charInfo/CharInfo.js
@@ -0,0 +1,131 @@
+import { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import './charInfo.scss';
+import MarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import Spinner from '../spinner/Spinner';
+import Skeleton from '../skeleton/Skeleton';
+
+class CharInfo extends Component {
+	state = {
+		char: null,
+		loading: false,
+		error: false,
+	};
+
+	marvelService = new MarvelService();
+
+	componentDidMount() {
+		this.updateChar();
+	}
+
+	componentDidUpdate(prevProps) {
+		if (this.props.charId !== prevProps.charId) this.updateChar();
+	}
+
+	updateChar = () => {
+		const { charId } = this.props;
+		if (!charId) {
+			return;
+		}
+
+		this.onCharLoadeding();
+
+		this.marvelService.getCharacter(charId).then(this.onCharLoaded).catch(this.onError);
+	};
+
+	onError = () => {
+		this.setState({
+			loading: false,
+			error: true,
+		});
+	};
+
+	onCharLoaded = (char) => {
+		this.setState({
+			char,
+			loading: false,
+		});
+	};
+
+	onCharLoadeding = () => {
+		this.setState({
+			loading: true,
+		});
+	};
+
+	render() {
+		const { char, loading, error } = this.state;
+
+		const skeleton = char || loading || error ? null : <Skeleton />;
+		const errorMessage = error ? <ErrorMessage /> : null;
+		const spinner = loading ? <Spinner /> : null;
+		const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+		return (
+			<div className="char__info">
+				{skeleton}
+				{errorMessage}
+				{spinner}
+				{content}
+			</div>
+		);
+	}
+}
+
+const View = ({ char }) => {
+	const { name, description, thumbnail, homepage, wiki, comics } = char;
+
+	let imgStyle = { objectFit: 'cover' };
+	if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+		imgStyle = { objectFit: 'contain' };
+	}
+
+	// if (comics.lenght > 5) {
+	// 	comics.lenght = 5;
+	// }
+
+	return (
+		<>
+			<div className="char__basics">
+				<img src={thumbnail} alt={name} style={imgStyle} />
+				<div>
+					<div className="char__info-name">{name}</div>
+					<div className="char__btns">
+						<a href={homepage} className="button button__main">
+							<div className="inner">homepage</div>
+						</a>
+						<a href={wiki} className="button button__secondary">
+							<div className="inner">Wiki</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="char__descr">{description}</div>
+			<div className="char__comics">Comics:</div>
+			<ul className="char__comics-list">
+				{comics.length > 0 ? null : 'No comics with this character'}
+				{
+					comics.map((item, i) => {
+						if (i > 9) return;
+
+						return (
+							<li key={i} className="char__comics-item">
+								{item.name}
+							</li>
+						);
+					})
+					/* .slice(0, 9) */
+				}
+			</ul>
+		</>
+	);
+};
+
+CharInfo.propTypes = {
+	charId: PropTypes.number,
+};
+
+export default CharInfo;
---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,21 @@
+import img from './error.gif';
+
+const ErrorMessage = () => {
+	return (
+		<img
+			alt="error"
+			src={img}
+			style={{
+				display: 'block',
+				width: '250px',
+				height: '250px',
+				objectFit: 'contain',
+				margin: '0 auto',
+			}}
+		/>
+	);
+};
+
+export default ErrorMessage;
+
+//process.env.PUBLIC_URL + '/error.gif'
